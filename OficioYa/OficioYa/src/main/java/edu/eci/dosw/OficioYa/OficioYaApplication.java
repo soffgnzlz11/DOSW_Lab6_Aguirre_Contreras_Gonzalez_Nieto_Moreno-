09bd22293690c8095fd464f6dac9fc8eaf74f612
unknown <mateo.moreno-t@labinfo.is.escuelaing.edu.co>
2026-09-18 08:16:49 -0500

    estructura inicial desarrolladad con spring initializr

---
+++ OficioYa/OficioYa/src/main/java/edu/eci/dosw/OficioYa/OficioYaApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.OficioYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficioYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficioYaApplication.class, args);
+	}
+
+}
